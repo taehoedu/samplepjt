cceefa5797b5a8f20eeb26f9479ff7c57863060d
taehoedu <taehoedu@gmail.com>
2024-11-21 12:55:14 +0900

    first commit

---
+++ src/main/java/com/office/samplepjt/SamplepjtApplication.java
@@ -0,0 +1,13 @@
+package com.office.samplepjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplepjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplepjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/office/samplepjt/SamplepjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.office.samplepjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplepjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
