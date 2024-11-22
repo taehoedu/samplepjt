3b8665bccafe6d808cc9082b6adeb3720fbcda47
taehoedu <taehoedu@gmail.com>
2024-11-22 14:36:11 +0900

    modify application.properties

--- src/main/java/com/office/samplepjt/HomeController.java
+++ src/main/java/com/office/samplepjt/HomeController.java
@@ -1,5 +1,6 @@
 package com.office.samplepjt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,9 +10,13 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 public class HomeController {
 
+	@Value("${server.name}")
+	private String serverName;
+	
 	@GetMapping({"", "/"})
 	public String home() {
 		log.info("home()");
+		log.info("serverName: {}", serverName);
 		
 		String nextPage = "home";
 		
